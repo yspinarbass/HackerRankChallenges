bec914da621ec0305e145eb7ce0a68748e538918
yspinarbass <106842078+yspinarbass@users.noreply.github.com>
2022-06-04 00:58:14 +0300

    Add files via upload

---
+++ Day_0_Variables.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) 
+    {
+        String inputString; 
+        
+        inputString = Console.ReadLine(); 
+      
+        Console.WriteLine("Hello, World.");
+        
+        Console.Write(inputString);
+    }
+}
---
+++ Day_2_Operators.cs
@@ -0,0 +1,40 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void solve(double meal_cost, int tip_percent, int tax_percent)
+    {
+        double tip_cost = meal_cost * tip_percent / 100;
+        double tax_cost = meal_cost * tax_percent / 100;
+        double total_cost = meal_cost + tip_cost + tax_cost;
+        
+        Console.WriteLine(Math.Round(total_cost));
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+        int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+    }
+}
